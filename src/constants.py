ef0086fa6f1df47fa7ffcc07946182a8a2a8aeb7
n2omatt <n2o.matt@gmail.com>
2016-06-18 03:20:36 -0300

    - Add the license headers. - Cosmetic changes. - Remove unneeded constants. - Add other constants.

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,43 @@
+##               █      █                                                     ##
+##               ████████                                                     ##
+##             ██        ██                                                   ##
+##            ███  █  █  ███        constants.py                              ##
+##            █ █        █ █        Game_RamIt                                ##
+##             ████████████                                                   ##
+##           █              █       Copyright (c) 2016                        ##
+##          █     █    █     █      AmazingCow - www.AmazingCow.com           ##
+##          █     █    █     █                                                ##
+##           █              █       N2OMatt - n2omatt@amazingcow.com          ##
+##             ████████████         www.amazingcow.com/n2omatt                ##
+##                                                                            ##
+##                  This software is licensed as GPLv3                        ##
+##                 CHECK THE COPYING FILE TO MORE DETAILS                     ##
+##                                                                            ##
+##    Permission is granted to anyone to use this software for any purpose,   ##
+##   including commercial applications, and to alter it and redistribute it   ##
+##               freely, subject to the following restrictions:               ##
+##                                                                            ##
+##     0. You **CANNOT** change the type of the license.                      ##
+##     1. The origin of this software must not be misrepresented;             ##
+##        you must not claim that you wrote the original software.            ##
+##     2. If you use this software in a product, an acknowledgment in the     ##
+##        product IS HIGHLY APPRECIATED, both in source and binary forms.     ##
+##        (See opensource.AmazingCow.com/acknowledgment.html for details).    ##
+##        If you will not acknowledge, just send us a email. We'll be         ##
+##        *VERY* happy to see our work being used by other people. :)         ##
+##        The email is: acknowledgment_opensource@AmazingCow.com              ##
+##     3. Altered source versions must be plainly marked as such,             ##
+##        and must not be misrepresented as being the original software.      ##
+##     4. This notice may not be removed or altered from any source           ##
+##        distribution.                                                       ##
+##     5. Most important, you must have fun. ;)                               ##
+##                                                                            ##
+##      Visit opensource.amazingcow.com for more open-source projects.        ##
+##                                                                            ##
+##                                  Enjoy :)                                  ##
+##----------------------------------------------------------------------------##
+
+
 START_LEVEL = 1;
 START_LIVES = 1;
 
@@ -6,7 +46,6 @@ GAME_STATE_PLAYING       = 1;
 GAME_STATE_VICTORY       = 3;
 GAME_STATE_DEFEAT        = 4;
 GAME_STATE_GAME_OVER     = 5;
-GAME_STATE_SPLASH_SCREEN = 6;
 
 ################################################################################
 ## DIRECTION                                                                  ##
@@ -87,11 +126,11 @@ PROJECTILE_SPEED  = 1000;
 ################################################################################
 ## COLORS                                                                     ##
 ################################################################################
-COLOR_TRANSPARENT = (0,0,0,0);
+COLOR_TRANSPARENT = (0, 0, 0, 0);
 COLOR_BLACK       = (0, 0, 0);
 COLOR_WHITE       = (255, 255, 255);
 COLOR_PIPE        = (160, 160, 160);
-
+COLOR_PLAYFIELD   = (162,   98, 33);
 import random;
 c = lambda x: (random.randint(40, 255), random.randint(40, 255), random.randint(40, 255));
 ENEMY_COLOR_INDEX = map(c, range(0, 500));
@@ -104,7 +143,7 @@ FONT_NAME = "nokiafc22.ttf";
 FONT_SIZE = 17;
 
 ################################################################################
-## TIMER T                                                         ##
+## TIMER                                                                      ##
 ################################################################################
 TIMER_ENEMY_BASE_TIME = 0.5;
 
@@ -113,7 +152,7 @@ TIMER_THRESHOLD = [
     [-1000],
 
     # LEVEL 1
-    [15, 10],
+    [20, 15, 10],
     # LEVEL 2
     [20, 15, 10],
     # COWTODO:
