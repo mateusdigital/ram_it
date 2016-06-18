956b416c2c7ea45771c0e7f830403d380d1551fa
n2omatt <n2o.matt@gmail.com>
2016-06-18 03:24:05 -0300

    - Add the license headers. - Change the blink rate. - Fix the set_blinking method. - Add an option to make the font antialiased.

--- src/text.py
+++ src/text.py
@@ -1,3 +1,41 @@
+##               █      █                                                     ##
+##               ████████                                                     ##
+##             ██        ██                                                   ##
+##            ███  █  █  ███        text.py                                   ##
+##            █ █        █ █        Game_RamIt                                ##
+##             ████████████                                                   ##
+##           █              █       Copyright (c) 2016                        ##
+##          █     █    █     █      AmazingCow - www.AmazingCow.com           ##
+##          █     █    █     █                                                ##
+##           █              █       N2OMatt - n2omatt@amazingcow.com          ##
+##             ████████████         www.amazingcow.com/n2omatt                ##
+##                                                                            ##
+##                  This software is licensed as GPLv3                        ##
+##                 CHECK THE COPYING FILE TO MORE DETAILS                     ##
+##                                                                            ##
+##    Permission is granted to anyone to use this software for any purpose,   ##
+##   including commercial applications, and to alter it and redistribute it   ##
+##               freely, subject to the following restrictions:               ##
+##                                                                            ##
+##     0. You **CANNOT** change the type of the license.                      ##
+##     1. The origin of this software must not be misrepresented;             ##
+##        you must not claim that you wrote the original software.            ##
+##     2. If you use this software in a product, an acknowledgment in the     ##
+##        product IS HIGHLY APPRECIATED, both in source and binary forms.     ##
+##        (See opensource.AmazingCow.com/acknowledgment.html for details).    ##
+##        If you will not acknowledge, just send us a email. We'll be         ##
+##        *VERY* happy to see our work being used by other people. :)         ##
+##        The email is: acknowledgment_opensource@AmazingCow.com              ##
+##     3. Altered source versions must be plainly marked as such,             ##
+##        and must not be misrepresented as being the original software.      ##
+##     4. This notice may not be removed or altered from any source           ##
+##        distribution.                                                       ##
+##     5. Most important, you must have fun. ;)                               ##
+##                                                                            ##
+##      Visit opensource.amazingcow.com for more open-source projects.        ##
+##                                                                            ##
+##                                  Enjoy :)                                  ##
+##----------------------------------------------------------------------------##
 ################################################################################
 ## Imports                                                                    ##
 ################################################################################
@@ -34,7 +72,7 @@ class Text:
 
         ## Blink timer
         self.visible     = True;
-        self.blink_timer = CowClock(0.3, CowClock.REPEAT_FOREVER,
+        self.blink_timer = CowClock(0.2, CowClock.REPEAT_FOREVER,
                                     self._on_blink_timer_tick);
 
 
@@ -49,8 +87,12 @@ class Text:
         self.contents = contents;
 
     def set_blinking(self, blink):
-        if(blink): self.blink_timer.start();
-        else     : self.blink_timer.stop ();
+        if(blink):
+            self.blink_timer.start();
+            self._on_blink_timer_tick();
+        else:
+            self.blink_timer.stop();
+            self.visible = True;
 
     def get_size(self, contents = None):
         if(contents == None):
@@ -66,9 +108,9 @@ class Text:
         self.blink_timer.update(dt);
 
 
-    def draw(self, surface):
+    def draw(self, surface, antialias = False):
         if(self.visible):
-            text_surface = self.font.render(self.contents, False, self.color);
+            text_surface = self.font.render(self.contents, antialias, self.color);
             surface.blit(text_surface, (self.x, self.y));
 
 
