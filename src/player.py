4db305196a5068545e78aaea17bac30fafc0d635
n2omatt <n2o.matt@gmail.com>
2016-06-18 03:22:27 -0300

    - Add the license headers. - Add comments. - Refactor some stuff. - Add sounds. - Use the new input module.

--- src/player.py
+++ src/player.py
@@ -1,3 +1,42 @@
+##               █      █                                                     ##
+##               ████████                                                     ##
+##             ██        ██                                                   ##
+##            ███  █  █  ███        player.py                                 ##
+##            █ █        █ █        Game_RamIt                                ##
+##             ████████████                                                   ##
+##           █              █       Copyright (c) 2016                        ##
+##          █     █    █     █      AmazingCow - www.AmazingCow.com           ##
+##          █     █    █     █                                                ##
+##           █              █       N2OMatt - n2omatt@amazingcow.com          ##
+##             ████████████         www.amazingcow.com/n2omatt                ##
+##                                                                            ##
+##                  This software is licensed as GPLv3                        ##
+##                 CHECK THE COPYING FILE TO MORE DETAILS                     ##
+##                                                                            ##
+##    Permission is granted to anyone to use this software for any purpose,   ##
+##   including commercial applications, and to alter it and redistribute it   ##
+##               freely, subject to the following restrictions:               ##
+##                                                                            ##
+##     0. You **CANNOT** change the type of the license.                      ##
+##     1. The origin of this software must not be misrepresented;             ##
+##        you must not claim that you wrote the original software.            ##
+##     2. If you use this software in a product, an acknowledgment in the     ##
+##        product IS HIGHLY APPRECIATED, both in source and binary forms.     ##
+##        (See opensource.AmazingCow.com/acknowledgment.html for details).    ##
+##        If you will not acknowledge, just send us a email. We'll be         ##
+##        *VERY* happy to see our work being used by other people. :)         ##
+##        The email is: acknowledgment_opensource@AmazingCow.com              ##
+##     3. Altered source versions must be plainly marked as such,             ##
+##        and must not be misrepresented as being the original software.      ##
+##     4. This notice may not be removed or altered from any source           ##
+##        distribution.                                                       ##
+##     5. Most important, you must have fun. ;)                               ##
+##                                                                            ##
+##      Visit opensource.amazingcow.com for more open-source projects.        ##
+##                                                                            ##
+##                                  Enjoy :)                                  ##
+##----------------------------------------------------------------------------##
+
 ################################################################################
 ## Imports                                                                    ##
 ################################################################################
@@ -6,8 +45,9 @@ import pygame;
 from pygame.locals import *;
 ## Game_RamIt ##
 import assets;
+import sound;
+import input;
 from constants import *;
-from input     import *;
 
 
 
@@ -49,13 +89,15 @@ class Player:
         return self.cannon_direction;
 
     def get_cannon_position(self):
-        #COWTODO: Fix this logic...
+        ## Cannon is not symmetric, so we need adjust the
+        ## projectile position a little bit.
+        center   = (self.width * 0.5);
+        offset_x = (center + PLAYER_CANNON_X_OFFSET);
+
         if(self.cannon_direction == DIRECTION_LEFT):
-            return (self.x - (self.width / 2 + PLAYER_CANNON_X_OFFSET),
-                    self.y + 4);
+            return (self.x - offset_x, self.y + 4);
         else:
-            return (self.x + (self.width / 2 + (PLAYER_CANNON_X_OFFSET + 10)),
-                    self.y + 4);
+            return ((self.x + offset_x + 10), self.y + 4);
 
     def wants_to_shoot(self):
         return self.should_shoot;
@@ -68,11 +110,16 @@ class Player:
     def update(self, dt):
         self._get_input();
 
-        self.y += (self.speed * dt);
+        new_y = self.y + (self.speed * dt);
 
         ## Maintain the player into bounds.
-        if  (self.y <= self.min_y): self.y = self.min_y;
-        elif(self.y >= self.max_y): self.y = self.max_y;
+        if  (new_y <= self.min_y): new_y = self.min_y;
+        elif(new_y >= self.max_y): new_y = self.max_y;
+
+        if(new_y != self.y):
+            self.y = new_y;
+            y_per = float(self.y - self.min_y) / (self.max_y - self.min_y);
+            sound.play_player_sound(y_per);
 
 
     ## Draw ####################################################################
@@ -102,18 +149,18 @@ class Player:
             self.x += PLAYER_CANNON_X_OFFSET;
 
 
-    ## Input ###################################################################
+    ## input ###################################################################
     def _get_input(self):
         self.speed = 0;
 
         ## Movement
-        if  (Input.is_down(K_UP  )): self.speed = -PLAYER_SPEED;
-        elif(Input.is_down(K_DOWN)): self.speed = +PLAYER_SPEED;
+        if  (input.is_down(K_UP  )): self.speed = -PLAYER_SPEED;
+        elif(input.is_down(K_DOWN)): self.speed = +PLAYER_SPEED;
 
         ## Cannon
-        if  (Input.is_down(K_LEFT )): self._set_cannon_dir(DIRECTION_LEFT );
-        elif(Input.is_down(K_RIGHT)): self._set_cannon_dir(DIRECTION_RIGHT);
+        if  (input.is_down(K_LEFT )): self._set_cannon_dir(DIRECTION_LEFT );
+        elif(input.is_down(K_RIGHT)): self._set_cannon_dir(DIRECTION_RIGHT);
 
         ## Shoot
-        self.should_shoot = Input.is_down(K_SPACE);
+        self.should_shoot = input.is_down(K_SPACE);
 
